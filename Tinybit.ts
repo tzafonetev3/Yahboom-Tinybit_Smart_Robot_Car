9c3a85a15a376e0ec681b79fa450c8527d345221
JasonLS <jsedluk01@gmail.com>
2019-07-30 12:31:02 -0400

    Update Tinybit.ts

--- Tinybit.ts
+++ Tinybit.ts
@@ -1,24 +1,41 @@
-let alreadyInit = 0
-const PWM_ADD = 0x01
-const MOTOR = 0x02
-const RGB = 0x01
-let yahStrip: neopixel.Strip;
-
+//////////////////////////////////////////////////////////////
+/*
+Copyright 2019 GHIElectronics, LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+//////////////////////////////////////////////////////////////
 //% weight=10 color=#006400 icon="\uf1b9" block="Tinybit"
-//% groups='["Motors", "Distance Sensor", "Line Reader","Lights", "Speaker", "Microphone"]'
+//% groups='["Motors", "Distance Sensor", "Line Reader","Lights", "Music", "Microphone"]'
+
+let SmartStrip: neopixel.Strip;
 
 namespace Tinybit {
+    const alreadyInit = 0
+    const I2C_ADDRESS = 0x01
+    const MOTOR = 0x02
+    const LeftLineSensorValue = pins.digitalReadPin(DigitalPin.P13)
+    const RightLineSensorValue = pins.digitalReadPin(DigitalPin.P14)
+
     export enum Motors {
         //% blockId="LeftMotor" block="LeftMotor"
-        LeftMotor = 0,
+        LeftMotor,
         //% blockId="RightMotor" block="RightMotor"
-        RightMotor = 1,
+        RightMotor,
         //% blockId="BothMotors" block="BothMotors"
-        BothMotors = 2
+        BothMotors
     }
+
     export enum direction {
-        forward = 1,
-        backward = 2
+        forward,
+        backward
     }
 
     export enum PingUnit {
@@ -27,62 +44,44 @@ namespace Tinybit {
         //% block="μs"
         MicroSeconds
     }
+
     export enum Linesensor {
         //% blockId="Left line reader" block="Left line reader"
-        LeftLineSensor = 13,
+        LeftLineSensor,
         //% blockId="Right line reader" block="Right line reader"
-        RightLineSensor = 14
+        RightLineSensor
     }
+
     export enum LEDpin {
         //% blockId="Headlight Left" block="Headlight Left"
-        HeadlightLeft = 8,
+        HeadlightLeft,
         //% blockId="Headlight Right" block="Headlight Right"
-        HeadlightRight = 12
+        HeadlightRight
     }
+    
     export enum LEDmode {
         //% blockId="ON" block="ON"
         ON = 0x01,
         //% blockId="OFF" block="OFF"
         OFF = 0x00
     }
+
     export enum linevalue {
         white,
         black
     }
-    export enum enMusic {
 
-        dadadum = 0,
-        entertainer,
-        prelude,
-        ode,
-        nyan,
-        ringtone,
-        funk,
-        blues,
-
-        birthday,
-        wedding,
-        funereal,
-        punchline,
-        baddy,
-        chase,
-        ba_ding,
-        wawawawaa,
-        jump_up,
-        jump_down,
-        power_up,
-        power_down
-    }
     function setPwmRGB(red: number, green: number, blue: number): void {
 
         let buf = pins.createBuffer(4);
-        buf[0] = RGB;
+        buf[0] = I2C_ADDRESS;
         buf[1] = red;
         buf[2] = green;
         buf[3] = blue;
 
-        pins.i2cWriteBuffer(PWM_ADD, buf);
+        pins.i2cWriteBuffer(I2C_ADDRESS, buf);
     }
+
     //% weight=100
     //% group="Motors"
     //% blockId=motor_MotorRun block="Set|%index|to|%Direction|at the speed|%speed" 
@@ -90,44 +89,30 @@ namespace Tinybit {
     //% speed.defl=100
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
     //% dir.fieldEditor="gridpicker" dir.fieldOptions.columns=2
-    export function MotorRun(index: Motors, dir: direction, speed: number): void {
-        speed = Math.round(speed * 2.55)
+    export function ControlMotors(WhichMotor: Motors, dir: direction, speed: number): void {
+        speed = Math.round(speed * 2.55);
         let buf = pins.createBuffer(5);
+
         buf[0] = MOTOR;
 
-
-        if (index == Motors.LeftMotor) {
+        if (WhichMotor == Motors.LeftMotor) {
             if (dir == direction.forward) {
-                //buf[1] = 0;
-                //buf[2] = 0;
                 buf[3] = speed;
-                //buf[4] = 0;
             }
             else if (dir == direction.backward) {
-                //buf[1] = 0;
-                //buf[2] = 0;
-                //buf[3] = 0;
                 buf[4] = speed;
-
             }
-
         }
-        else if (index == Motors.RightMotor) {
+        else if (WhichMotor == Motors.RightMotor) {
             if (dir == direction.forward) {
                 buf[1] = speed;
-                //buf[2] = 0;
-                //buf[3] = 0;
-                //buf[4] = 0;
             }
             else if (dir == direction.backward) {
-                //buf[1] = 0;
                 buf[2] = speed;
-                //buf[3] = 0;
-                //buf[4] = 0;
             }
-        }
 
-        else if (index == Motors.BothMotors) {
+        }
+        else if (WhichMotor == Motors.BothMotors) {
             if (dir == direction.forward) {
                 buf[1] = speed;
                 buf[2] = 0;
@@ -139,36 +124,36 @@ namespace Tinybit {
                 buf[2] = speed;
                 buf[3] = 0;
                 buf[4] = speed;
-
             }
 
         }
-        pins.i2cWriteBuffer(PWM_ADD, buf);
 
+        pins.i2cWriteBuffer(I2C_ADDRESS, buf);
     }
+
     //% weight=98
     //% group="Motors"
-    //% blockId=motor_motorStop block="Stop|%motors"
+    //% blockId=motorStop block="Stop motors"
     //% motors.fieldEditor="gridpicker" motors.fieldOptions.columns=2
-    export function motorStop(motors: Motors): void {
+    export function motorStop(): void {
         let buf = pins.createBuffer(5);
+
         buf[0] = MOTOR;
         buf[1] = 0;
         buf[2] = 0;
         buf[3] = 0;
         buf[4] = 0;
 
-        pins.i2cWriteBuffer(PWM_ADD, buf);
-
+        pins.i2cWriteBuffer(I2C_ADDRESS, buf);
     }
 
     //% group="Distance Sensor"
-    //% blockId=mbit_ultrasonic_car block="distance sensor value in %unit"
+    //% blockId=ReadUltrasonicSensor block="distance sensor value in centimeters"
     //% color="#006400"
     //% weight=98
     //% blockGap=10
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function Ultrasonic(unit: PingUnit, maxCmDistance = 500): number {
+    export function ReadUltrasonicSensor(): number {
         // send pulse
         pins.setPull(DigitalPin.P16, PinPullMode.PullNone);
         pins.digitalWritePin(DigitalPin.P16, 0);
@@ -176,141 +161,111 @@ namespace Tinybit {
         pins.digitalWritePin(DigitalPin.P16, 1);
         control.waitMicros(15);
         pins.digitalWritePin(DigitalPin.P16, 0);
+
         // read pulse
         pins.setPull(DigitalPin.P15, PinPullMode.PullUp);
 
-        let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, maxCmDistance * 42);
-        let dr = Math.round(d / 42);
-        console.log("Distance: " + dr);
+        let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 21000);
+
+        d = Math.round(d / 42);
+
+        console.log("Distance: " + d);
 
         basic.pause(50)
 
-        switch (unit) {
-            case PingUnit.Centimeters: return dr;
-            default: return dr;
-        }
-
+        return d;
     }
-
 
     //% weight=89
     //% group="Line Reader"
-    //% blockId=read_Linesensor block=" %Linesensor detects %type"
+    //% color="#006400"
+    //% blockId=ReadLinereaderValue block=" %Linesensor detects %type"
     //% Linesensor.fieldEditor="gridpicker" Linesensor.fieldOptions.columns=2 
+    export function ReadLinereaderValue(Line: Linesensor, typeline: linevalue): boolean {
+        let pin = Line === Linesensor.LeftLineSensor ? DigitalPin.P13 : DigitalPin.P14
 
+        if (pins.digitalReadPin(pin) === 0) {
 
-    export function readlinereadervalue(Line: Linesensor, typeline: linevalue): boolean {
-        let LeftLineSensorValue = pins.digitalReadPin(DigitalPin.P13)
-        let RightLineSensorValue = pins.digitalReadPin(DigitalPin.P14)
-        if (typeline == linevalue.white && LeftLineSensorValue == 0) {
-            return true
-        }
-        else if (typeline == linevalue.black && LeftLineSensorValue == 1) {
-            return true
-        }
-        if (typeline == linevalue.white && RightLineSensorValue == 0) {
-            return true
-        }
-        else if (typeline == linevalue.black && RightLineSensorValue == 1) {
-            return true
+            return typeline === linevalue.white;
         }
         else {
-            return false
+
+            return typeline === linevalue.black;
+        }
+        if (pins.digitalReadPin(pin) === 0) {
+
+            return typeline === linevalue.white;
+        }
+        else {
+
+            return typeline === linevalue.black;
         }
     }
 
-    //% weight=87
-    //% group="Lights"
-    //% blockId=writeLED block="Set|%led|to|%ledswitch"
-    //% led.fieldEditor="gridpicker" led.fieldOptions.columns=2 
-    //% ledswitch.fieldEditor="gridpicker" ledswitch.fieldOptions.columns=2
-    export function writeLED(led: LEDpin, ledswitch: LEDmode): void {
-        if (led == LEDpin.HeadlightLeft) {
-            pins.digitalWritePin(DigitalPin.P8, ledswitch)
-        } else if (led == LEDpin.HeadlightRight) {
-            pins.digitalWritePin(DigitalPin.P12, ledswitch)
-        } else {
-            return
-
-        }
-    }
     //% weight=10
     //% inlineInputMode=inline
-    //% blockId=RGB block="Set Lights to Red:%r Green:%g Blue:%b"
+    //% blockId=rgblight block="Set headlights to Red:%r Green:%g Blue:%b"
     //% r.min=0 r.max=100
     //% g.min=0 g.max=100
     //% b.min=0 b.max=100
     //% group="Lights"
+    //% color="#006400"
     export function rgblight(r: number, g: number, b: number): void {
         //scaling since 255 wont mean anything to a teacher/student 
         //User sees a value of 100 while program sees the value 255
-        let R = r * 2.55;
-        let G = g * 2.55;
-        let B = b * 2.55;
-        if (R > 255)
-            R = 255;
-        if (G > 255)
-            G = 255;
-        if (B > 255)
-            B = 255;
+        r *= 2.55;
+        g *= 2.55;
+        b *= 2.55;
 
-        setPwmRGB(R, G, B);
+        if (r > 255) r = 255;
+        if (g > 255) g = 255;
+        if (b > 255) b = 255;
 
+        setPwmRGB(r, g, b);
     }
-    //% blockId=Tinybit_RGB_Car_Program block="Smart Leds"
+
+    //% weight=5
+    //% inlineInputMode=inline
+    //% blockId=rgblightoff block="Turn off headlights"
+    //% group="Lights"
+    //% color="#006400"
+    export function rgblightoff(): void {
+        setPwmRGB(0, 0, 0)
+    }
+
+    //% blockId=RGB_Neopixel block="Smart Leds"
     //% weight=99
     //% blockGap=10
     //% color="#006400"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     //% group="Lights"
-    export function RGB_Car_Program(): neopixel.Strip {
-
-        if (!yahStrip) {
-            yahStrip = neopixel.create(DigitalPin.P12, 2, NeoPixelMode.RGB);
+    export function RGB_Neopixel(): neopixel.Strip {
+        if (SmartStrip === null || SmartStrip === undefined) {
+            SmartStrip = neopixel.create(DigitalPin.P12, 2, NeoPixelMode.RGB);
         }
-        return yahStrip;
+        return SmartStrip;
     }
-    //% blockId=Tinybit_Music_Car block="Play sound %index"
+
+    //% blockId=Music_Car block="Play sound %index"
     //% weight=95
     //% blockGap=10
     //% color="#006400"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    //% group="Speaker"
-    export function Music_Car(index: enMusic): void {
-        switch (index) {
-            case enMusic.dadadum: music.beginMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once); break;
-            case enMusic.birthday: music.beginMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once); break;
-            case enMusic.entertainer: music.beginMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once); break;
-            case enMusic.prelude: music.beginMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once); break;
-            case enMusic.ode: music.beginMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once); break;
-            case enMusic.nyan: music.beginMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once); break;
-            case enMusic.ringtone: music.beginMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once); break;
-            case enMusic.funk: music.beginMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Once); break;
-            case enMusic.blues: music.beginMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once); break;
-            case enMusic.wedding: music.beginMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Once); break;
-            case enMusic.funereal: music.beginMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once); break;
-            case enMusic.punchline: music.beginMelody(music.builtInMelody(Melodies.Punchline), MelodyOptions.Once); break;
-            case enMusic.baddy: music.beginMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once); break;
-            case enMusic.chase: music.beginMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once); break;
-            case enMusic.ba_ding: music.beginMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once); break;
-            case enMusic.wawawawaa: music.beginMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once); break;
-            case enMusic.jump_up: music.beginMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once); break;
-            case enMusic.jump_down: music.beginMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Once); break;
-            case enMusic.power_up: music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once); break;
-            case enMusic.power_down: music.beginMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once); break;
-        }
+    //% group="Music"
+    export function Music_Car(melody: Melodies): void {
+        music.beginMelody(music.builtInMelody(melody), MelodyOptions.Once);
     }
-    //% blockId=Tinybit_Voice_Sensor block="a noise is made"
+
+    //% blockId=Read_Voice_Sensor block="a noise is made"
     //% weight=60
     //% blockGap=10
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=12
     //% group="Microphone"
-    export function Voice_Sensor(): boolean {
+    export function Read_Voice_Sensor(): boolean {
         //pins.setPull(DigitalPin.P1, PinPullMode.PullUp);
-        let temp = 0;
-        temp = pins.analogReadPin(AnalogPin.P1);
+        let SoundValue = pins.analogReadPin(AnalogPin.P1);
         //Sound detected
-        if (temp > 150) {
+        if (SoundValue > 100) {
             return true
         }
         else {
